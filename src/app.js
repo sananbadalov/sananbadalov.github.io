7e6c2e4b8e05a1251c59dc95ecec8afa035df376
sananbadalov <coder.hs110@gmail.com>
2022-04-05 01:15:58 +0400

    Brand New Site

--- src/app.js
+++
@@ -1,8 +0,0 @@
-const hambButton = document.getElementById('hamb')
-const navList = document.getElementById('nav-list')
-
-function toggleButton() {
-    navList.classList.toggle('show')
-}
-
-hambButton.addEventListener('click', toggleButton)
